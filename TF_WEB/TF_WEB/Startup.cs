217c8ba71424a49481887f3455fe53865e28f008
sriosGit <srios0u@gmail.com>
2016-10-24 14:52:11 -0500

    Trabajo Final Web

---
+++ TF_WEB/TF_WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TF_WEB.Startup))]
+namespace TF_WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
